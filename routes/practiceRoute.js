d3e22546e5ef0f3a42961e8c1a5ad8478575105b
ashif945 <ansariashif945>
2026-09-14 11:26:28 +0530

    Initial Commit

---
+++ routes/practiceRoute.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const {getStudentsOlder21, 
+    getMJStudents, 
+    getStudentsStartsWithA, 
+    countStudentsByCourse, getStudentsAvgAgeByCourse,
+    sortStudentsByAge, createIndexOnEmail,
+    explainEmailQuery,createStudent, getStudentById
+    } = require("../controllers/practiceController")
+
+const router = express.Router();
+
+router.get("/getStudentsOT21", getStudentsOlder21)
+router.get("/getMJStudents", getMJStudents)
+router.get("/getStudentsStartsWithA", getStudentsStartsWithA)
+router.get("/countStudentsByCourse", countStudentsByCourse)
+router.get("/getStudentsAvgAgeByCourse", getStudentsAvgAgeByCourse)
+router.get("/sortStudentsByAge", sortStudentsByAge)
+router.get("/explainEmailQuery", explainEmailQuery)
+router.get("/getStudentById/:id", getStudentById)
+
+router.post("/createIndexOnEmail", createIndexOnEmail)
+router.post("/createStudent", createStudent)
+
+module.exports = router;
