00f2b7862c7aaa43e78c1ac3e3205a9f67d01031
ashif945 <ansariashif945>
2026-09-17 09:52:04 +0530

    access and refresh tokens

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { generateAccessToken, generateRefreshToken } = require("../utils/tokenUtils");
 
 const register = async (req, res) => {
   try {
@@ -74,16 +75,14 @@ const login = async(req, res) => {
       });
     }
 
-    const accessToken = jwt.sign(
-      { userId: user._id.toString(), role: user.role},
-      process.env.JWT_SECRET,
-      { expiresIn: "1d"}
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
 
     res.status(200).json({
       success: true,
       message: "Login Successful",
       accessToken,
+      refreshToken,
       user: {
         id: user._id,
         name: user.name,
@@ -100,5 +99,42 @@ const login = async(req, res) => {
     })
   }
 }
+
+const refreshAccessToken = async(req, res) => {
+  try{
+    const {refreshToken} = req.body;
+
+    if(!refreshToken){
+      return res.status(401).json({
+        success: false,
+        message: "refresh token is required"
+      })
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+    const user = await User.findById(decoded.userId);
+
+    if(!user){
+      return res.status(401).json({
+        success: false,
+        message: "User no longer exists"
+      });
+    }
+
+    const newAccessToken = generateAccessToken(user);
+    res.status(200).json({
+      success: true,
+      accessToken: newAccessToken
+    })
+
+  } catch(err){
+    console.err(err);
+
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired refresh token"
+    });
+  }
+} 
 
 module.exports = { register, login };
